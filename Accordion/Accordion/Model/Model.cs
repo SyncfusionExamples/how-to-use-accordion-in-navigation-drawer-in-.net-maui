0f6d654d0329fce2d937db95d208ebba8ca4cd5f
SethupathyD <sethupathy.d@syncfusion.com>
2025-04-16 19:03:40 +0530

    code updated.

---
+++ Accordion/Accordion/Model/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accordion
+{
+    public class ItemInfo
+    {
+        #region Constructor
+
+        public ItemInfo()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        #endregion
+    }
+}
